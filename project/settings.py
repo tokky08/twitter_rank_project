4b460e2943e40ae295400030c908742b82368913
TOKITA KAZUHITO <tokky.kz08@gmail.com>
2020-03-24 13:28:18 +0900

    ALLOWED_HOSTS = ['*.herokuapp.com']

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = False
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['*.herokuapp.com']
 
 
 # Application definition
